a86df4ced4a47c887ef859b78bc95e28566347c6
daydash <thedaydash@gmail.com>
2023-07-05 18:37:38 +0530

    2_NmG52qrm_added_share_to_whatsapp

--- script.js
+++ script.js
@@ -3,43 +3,55 @@ const quotes = [
     id: 0,
     quote:
       "All our dreams can come true, if we have the courage to pursue them.",
+
+    color: "#19d480",
   },
   {
     id: 1,
     quote: "Wake up with determination. Go to bed with satisfaction.",
+    color: "#000000",
   },
   {
     id: 2,
     quote: "It's going to be hard, but hard does not mean impossible.",
+    color: "#5bfacd",
   },
   {
     id: 3,
     quote: "Dream it. Believe it. Build it.",
+    color: "#7346f5",
   },
   {
     id: 4,
     quote:
       "The harder you work for something, the greater you'll feel when you achieve it.",
+
+    color: "#34f201",
   },
   {
     id: 5,
     quote: "Success doesn't just find you. You have to go out and get it.",
+    color: "#f93f7f",
   },
   {
     id: 6,
     quote: "Dream it. Wish it. Do it.",
+    color: "#638cad",
   },
   {
     id: 7,
     quote: "Great things never come from comfort zones.",
+    color: "#110e41",
   },
   {
     id: 8,
     quote: "Push yourself, because no one else is going to do it for you.",
+    color: "#cc6c46",
   },
   {
     id: 9,
     quote: "If you cannot to greate things, do small things in greate way.",
+    color: "#dd81f4",
   },
 ];
 
@@ -72,9 +84,19 @@ const setQuote = () => {
   if (unusedIds.length !== 0) {
     let id = Math.floor(Math.random() * unusedIds.length);
     p.innerText = quotes[unusedIds[id]].quote;
+    document.querySelector("body").style.background =
+      quotes[unusedIds[id]].color;
+    document.querySelector(".container").style.background =
+      quotes[unusedIds[id]].color;
+    document.querySelector("#whatsapp-link").href = `https://wa.me/?text=${
+      quotes[unusedIds[id]].quote
+    }`;
     unusedIds.splice(id, 1);
   } else if (unusedIds.length === 0) {
     unusedIds = quotes.map((x) => x.id);
     setQuote();
   }
 };
+
+// data-action="share/whatsapp/share"
+//         target="_blank"
